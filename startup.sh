735a1c1259a70a0c210c1124f14aa11a4c78c9d7
Matt Harp <matthewharp@gmail.com>
2025-09-04 15:13:06 -0500

    wip

--- startup.sh
+++ startup.sh
@@ -1,1 +1,1 @@
-python3 manage.py collectstatic && gunicorn --workers 2 the_fasting_ox_python_django_api.wsgi.application
+python manage.py collectstatic && gunicorn --workers 2 the_fasting_ox_python_django_api.wsgi
