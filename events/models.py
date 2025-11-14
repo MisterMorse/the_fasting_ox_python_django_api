fdd26240cf806ef38383ec1472efea3eed259f90
Matt Harp <79379521+MisterMorse@users.noreply.github.com>
2025-11-14 16:19:02 -0600

    Created the mechanics for storage and retrieval of events.
    The Event model is the first one I wanted to actually pull from the database, and this is all of the machinery.

---
+++ events/models.py
@@ -0,0 +1,20 @@
+from django.db import models
+
+
+class Event(models.Model):
+    category = models.CharField(max_length=100)
+    name = models.CharField(max_length=100)
+    description = models.TextField()
+    location = models.CharField(max_length=100)
+    date = models.DateField()
+    start_time = models.TimeField()
+    end_time = models.TimeField()
+    day_of_week = models.CharField(max_length=100)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+
+    class Meta:
+        ordering = [ "date", "start_time", "name", "id" ]
+
+    def __str__(self):
+        return f"{ self.category } -> { self.name }, { self.date }"
