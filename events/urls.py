e2e769b7602219ede0cd5a50fc081289f9298aa5
Matt Harp <79379521+MisterMorse@users.noreply.github.com>
2025-12-17 15:49:49 -0600

    Added the ability to fetch events in bulk by category.
    Added a new API endpoint to allow bulk event fetch by specific category. Client needs to be able to narrow down the search, and the new endpoint should provide that ability.

--- events/urls.py
+++ events/urls.py
@@ -3,5 +3,6 @@ from django.urls import path
 from events import views
 
 urlpatterns = [
-    path("", views.event_list)
+    path("", views.event_list),
+    path("<str:category>/", views.event_list_by_category),
 ]
