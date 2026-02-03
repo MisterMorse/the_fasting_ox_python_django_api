15bf57fb80867ab29f626621c238bae8cffde364
Matt Harp <79379521+MisterMorse@users.noreply.github.com>
2026-02-03 15:56:49 -0600

    Pruned health checks.
    Consolidated deployment and database health checks into one endpoint to better fit AWS scheme.

--- health/urls.py
+++ health/urls.py
@@ -3,6 +3,5 @@ from django.urls import path
 from health import views
 
 urlpatterns = [
-    path("deployment/", views.health_deployment),
-    path("database/", views.health_database),
+    path("/", views.health),
 ]
